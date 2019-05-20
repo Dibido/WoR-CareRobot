4b3440e9e2babbde07cb99bccca3e57b78628c83
Brandon Geldof <BD.Geldof@student.han.nl>
2019-05-20 11:01:40 +0200

    [EBGURG-292] Clang-format test

--- kinematica/robotcontroller/include/robotcontroller/RobotControlPublisher.hpp
+++ kinematica/robotcontroller/include/robotcontroller/RobotControlPublisher.hpp
@@ -3,46 +3,48 @@
 
 #include "kinematics/DenavitHartenberg.hpp"
 #include "kinematics/UtilityFunctions.hpp"
-#include "robotcontroller_msgs/Control.h"   
+#include "robotcontroller_msgs/Control.h"
 #include "ros/ros.h"
 
 #include <iostream>
 
-namespace robotcontroller {
-
-/**
- * 
- * @author Brandon Geldof
- * @brief Publisher for controlling the robotarm
- * 
- */
-class RobotControlPublisher
+namespace robotcontroller
 {
-    public:
+
+  /**
+   *
+   * @author Brandon Geldof
+   * @brief Publisher for controlling the robotarm
+   *
+   */
+  class RobotControlPublisher
+  {
+      public:
     /**
      * @brief Construct a new Robot Control Publisher object
-     * 
-     * @param lN NodeHandler 
+     *
+     * @param lN NodeHandler
      * @param lTopic Topic on which the message will be published
      * @param lQue_size Number of messages that will be qued
      */
-    explicit RobotControlPublisher(ros::NodeHandle& lN, const std::string& lTopic, 
-    const uint16_t lQue_size);
+    explicit RobotControlPublisher(ros::NodeHandle& lN,
+                                   const std::string& lTopic,
+                                   const uint16_t lQue_size);
 
     /**
      * @brief Publishes a Control msg to the robot_command topic
-     * 
+     *
      * @param lSf Speedfactor for the robotarm movement speed
      * @param lConfiguration Configuration of the robotarm
      */
     void publish(const double lSf, const std::vector<double>& lConfiguration);
 
-    private:
+      private:
     ros::NodeHandle& mN;
     const std::string& cTopic;
     const uint16_t cQue_size;
     ros::Publisher mRobotControl_pub;
-};
-}
+  };
+} // namespace robotcontroller
 
-#endif //ROBOTCONTROL_PUBLISHER_HPP
+#endif // ROBOTCONTROL_PUBLISHER_HPP
