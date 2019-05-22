9d36413a05fa3b332c420ce71f19d32bac97b93c
Dibran Dokter <HS.Dokter@student.han.nl>
2019-05-22 16:50:25 +0200

    EBGURG-360: Added lidarData unittest

--- simulation/sim_lidar/include/sim_lidar/LidarPlugin.hpp
+++ simulation/sim_lidar/include/sim_lidar/LidarPlugin.hpp
@@ -1,49 +1,72 @@
-
+#ifndef LIDAR_PLUGIN_HPP
+#define LIDAR_PLUGIN_HPP
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/plugins/RayPlugin.hh>
+
+#include <sensor_interfaces/LidarData.h>
+#include <sensor_msgs/LaserScan.h>
 
 #include <ros/ros.h>
 
 namespace gazebo
 {
-class LidarPlugin : public RayPlugin
-{
-public:
-  LidarPlugin() = default;
+  class LidarPlugin : public RayPlugin
+  {
+      public:
+    LidarPlugin() = default;
 
-  virtual ~LidarPlugin() = default;
+    virtual ~LidarPlugin() = default;
 
-  /**
-   * @brief Load the robot controller plugin, overrides the Load from ModelPlugin
-   * @param aParent: parent model
-   * @param aSdf: the sdf (xml) in the robot model, within the <plugin> element
-   */
-  void Load(sensors::SensorPtr aParent, sdf::ElementPtr aSdf);
+    /**
+     * @brief Load the robot controller plugin, overrides the Load from
+     * ModelPlugin
+     * @param aParent: parent model
+     * @param aSdf: the sdf (xml) in the robot model, within the <plugin>
+     * element
+     */
+    void Load(sensors::SensorPtr aParent, sdf::ElementPtr aSdf);
 
-private:
-  /**
-   * @brief Callback function of gazebo sensor topic
-   *        Publishes a LaserScan message on ros topic @var: topicName
-   *        Publishes a LidarData message on ros topic /sensor/lidardata
-   * @param aMsg: contains the information of the sensor
-   */
-  void OnScan(ConstLaserScanStampedPtr& aMsg);
+      private:
+    /**
+     * @brief Callback function of gazebo sensor topic
+     *        Publishes a LaserScan message on ros topic @var: topicName
+     *        Publishes a LidarData message on ros topic /sensor/lidardata
+     * @param aMsg: contains the information of the sensor
+     */
+    void OnScan(ConstLaserScanStampedPtr& aMsg);
 
-  // Variables
-  std::string mFrameName;
-  std::string mTopicName;
-  std::string mLidarDataTopic;
-  sensors::RaySensorPtr mParentSensor;
+    /**
+     * @brief
+     *
+     */
+    sensor_msgs::LaserScan convertToLaserScan(ConstLaserScanStampedPtr& aMsg);
 
-  // Gazebo variables
-  transport::NodePtr mGazeboNode;
-  transport::SubscriberPtr mGazeboSub;
+    /**
+     * @brief
+     *
+     * @param aMsg
+     * @return sensor_interfaces::LidarData
+     */
+    sensor_interfaces::LidarData
+        convertToLidarData(ConstLaserScanStampedPtr& aMsg);
 
-  // Ros variables
-  ros::NodeHandlePtr mRosNode;
-  ros::Publisher mRosPub;
-  ros::Publisher mLidarDataPub;
-};
-GZ_REGISTER_SENSOR_PLUGIN(LidarPlugin)
-}  // namespace gazebo
+    // Variables
+    std::string mFrameName;
+    std::string mTopicName;
+    std::string mLidarDataTopic;
+    sensors::RaySensorPtr mParentSensor;
+
+    // Gazebo variables
+    transport::NodePtr mGazeboNode;
+    transport::SubscriberPtr mGazeboSub;
+
+    // Ros variables
+    ros::NodeHandlePtr mRosNode;
+    ros::Publisher mRosPub;
+    ros::Publisher mLidarDataPub;
+  };
+  GZ_REGISTER_SENSOR_PLUGIN(LidarPlugin)
+} // namespace gazebo
+
+#endif
--- simulation/sim_lidar/unitTests/gtest.cpp
+++ simulation/sim_lidar/unitTests/gtest.cpp
@@ -1,35 +1,103 @@
 // Bring in my package's API, which is what I'm testing
+#include <boost/shared_ptr.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/plugins/RayPlugin.hh>
 #include <ros/ros.h>
-#include <sim_lidar/LidarPlugin.h>
 // Include messages
+#include <sensor_interfaces/LidarData.h>
 #include <sensor_msgs/LaserScan.h>
-#include <sensor_interfaces/LidarData.hpp>
 // Bring in gtest
 #include <gtest/gtest.h>
+#include <iostream>
+#include <string.h>
 
-// Declare a test
-TEST(TestProtocol, TestLidarData)
+#include <type_traits>
+// Make everything public in to test code.
+#define private public
+#include <sim_lidar/LidarPlugin.hpp>
+#undef private
+
+/**
+ * @brief Test the protocol LidarDataConversion
+ * Check whether the data that is sent to the lidar plugin is converted
+ * properly.
+ */
+TEST(LidarData, LidarDataConversion)
 {
-  LidarPlugin lLidarPLugin;
-  // Publish data to the sensor topic
-  ros::Publisher lDataPublisher = nh.advertise("/sensor/lidar/scan", 1);
+  // Set values
+  gazebo::LidarPlugin lLidarPlugin;
+  const unsigned int lNumberOfMeasurements = 360;
+  // Set up message
+  // ConstLaserScanStampedPtr
   sensor_msgs::LaserScan lLaserScanMessage;
-  lLaserScanMessage.
-  lDataPublisher.publish(lLaserScanMessage);
-  // Read the output topic
-  ros::Subscriber lDataSubscriber = nh.subscribe("/sensor/lidar", 1);
-  gazeboSub = nh->Subscribe(parentSensor->Topic(), &LidarPlugin::OnScan, this);
-  // Check the size
-  EXPECT_EQ();
-  // Compare the values
-  EXPECT_EQ(1000, 1000);
+  lLaserScanMessage.time_increment = 2;
+  lLaserScanMessage.angle_min = static_cast<float>(-M_PI);
+  lLaserScanMessage.angle_max = static_cast<float>(M_PI);
+  lLaserScanMessage.range_min = static_cast<float>(0.1);
+  lLaserScanMessage.range_max = 3;
+  lLaserScanMessage.ranges.clear();
+  lLaserScanMessage.intensities.clear();
+  for (unsigned int i = 0; i < lNumberOfMeasurements; i++)
+  {
+    lLaserScanMessage.ranges.push_back(2);
+    lLaserScanMessage.intensities.push_back(0);
+  }
+  // Call parser function
+  sensor_interfaces::LidarData lLidarMessage;
+  lLidarMessage = lLidarPlugin.convertToLidarData(lLaserScanMessage);
+  // Check size
+  ASSERT_EQ(lLidarMessage.distances.size(), lLaserScanMessage.ranges.size());
+  // Check conversion
+  EXPECT_EQ(lLidarMessage.header.frame_id, lLaserScanMessage.header.frame_id);
+  EXPECT_EQ(lLidarMessage.measurement_angles.at(0), 0.0);
+  EXPECT_EQ(lLidarMessage.measurement_angles.at(
+                lLidarMessage.measurement_angles.size()),
+            (M_PI * 2));
+  EXPECT_EQ(lLidarMessage.distances, lLaserScanMessage.ranges);
+  float lExpectedAngle = 0;
+  for (unsigned int i = 0; i < lLidarMessage.distances.size(); i++)
+  {
+    lExpectedAngle += lLaserScanMessage.angle_increment;
+    EXPECT_EQ(lLidarMessage.measurement_angles.at(i), lExpectedAngle);
+  }
+}
+
+/**
+ * @brief Test the protocol ScanDataConversion
+ * Check whether the data that is sent to the ScanData topic is converted
+ * properly.
+ */
+TEST(LidarData, ScanDataConversion)
+{
+  // Set values
+  gazebo::LidarPlugin lLidarPlugin;
+  const unsigned int lNumberOfMeasurements = 360;
+  // Set up message
+  sensor_msgs::LaserScan lLaserScanMessage;
+  lLaserScanMessage.time_increment = 2;
+  lLaserScanMessage.angle_min = static_cast<float>(-M_PI);
+  lLaserScanMessage.angle_max = static_cast<float>(M_PI);
+  lLaserScanMessage.range_min = static_cast<float>(0.1);
+  lLaserScanMessage.range_max = 3;
+  lLaserScanMessage.ranges.clear();
+  lLaserScanMessage.intensities.clear();
+  for (unsigned int i = 0; i < lNumberOfMeasurements; i++)
+  {
+    lLaserScanMessage.ranges.push_back(2);
+    lLaserScanMessage.intensities.push_back(0);
+  }
+  // Call parser function
+  gazebo_msgs::LaserScan lLaserScanDataMessage;
+  lLaserScanDataMessage = lLidarPlugin.convertToLaserScan(lLaserScanMessage);
+  // Check conversion
+  // TODO
 }
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char** argv)
 {
-		  testing::InitGoogleTest(&argc, argv);
-		  ros::init(argc, argv, "tester");
-		  ros::NodeHandle nh;
-		  return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "LidarData_tester");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
 }
