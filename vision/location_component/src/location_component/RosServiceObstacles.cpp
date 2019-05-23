74b0b376ea850f45e4d5acc98b6d7bfc01ba50c2
Hendrik Nusselder <dh.nusselder@student.han.nl>
2019-05-23 16:33:03 +0200

    EBGURG-336 Implemented iterface

---
+++ vision/location_component/src/location_component/RosServiceObstacles.cpp
@@ -0,0 +1,10 @@
+#include "location_component/RosServiceObstacles.hpp"
+
+namespace location_component
+{
+
+  void RosServiceObstacles::parseObstacles(const environment_controller::Obstacles& aObstacles)
+  {
+    
+  }
+} 
