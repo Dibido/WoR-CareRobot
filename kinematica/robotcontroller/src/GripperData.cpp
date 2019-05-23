1a1eae2d5c20d2ffd0f711f5bf9ccfadb8b82ef2
Brandon Geldof <BD.Geldof@student.han.nl>
2019-05-23 09:19:15 +0200

    [EBGURG-367] Fixed checks

--- kinematica/robotcontroller/src/GripperData.cpp
+++ kinematica/robotcontroller/src/GripperData.cpp
@@ -15,22 +15,22 @@ namespace robotcontroller
         cEpsilon_inner_m(anEpsilonInner_m),
         cEpsilon_outer_m(anEpsilonInner_m)
   {
-    if (aWidth_m > 0.0 && aWidth_m <= cMaxWidth_m)
+    if (aWidth_m < 0.0 || aWidth_m > cMaxWidth_m)
       throw std::invalid_argument("Width_m must be between 0.0 and 0.08: " +
                                   std::to_string(aWidth_m));
 
-    if (aSpeedfactor > 0.0 && aSpeedfactor <= cMaxSpeedfactor)
+    if (aSpeedfactor < 0.0 || aSpeedfactor > cMaxSpeedfactor)
       throw std::invalid_argument("Speedfactor must be between 0.0 and 1.0: " +
                                   std::to_string(aSpeedfactor));
 
     // if(aForce_nm) No check yet. At the moment it's not clear what the ranges
     // are for the force
 
-    if (anEpsilon_inner_m > 0)
+    if (anEpsilon_inner_m < 0)
       throw std::invalid_argument("Epsilon_inner_m must be above 0.0: " +
                                   std::to_string(anEpsilonInner_m));
 
-    if (anEpsilon_outer_m > 0)
+    if (anEpsilon_outer_m < 0)
       throw std::invalid_argument("Epsilon_outer_m must be above 0.0: " +
                                   std::to_string(anEpsilonOuter_m));
   }
