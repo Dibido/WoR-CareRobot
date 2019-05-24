9d7be37762925d9ffe055994043a7ebfe1981bd0
Ivo Hulsman <I.Hulsman@student.han.nl>
2019-05-24 10:52:19 +0200

    EBGURG-316 - Added doxygen

--- vision/userinterface/include/mainwindow.hpp
+++ vision/userinterface/include/mainwindow.hpp
@@ -13,11 +13,25 @@ class MainWindow : public QMainWindow
   Q_OBJECT
 
     public:
+  /**
+   * @brief Default QT Constructor
+   *
+   * @param parent
+   * Ctor calls setupUi() which creates the GUI screen(s).
+   */
   explicit MainWindow(QWidget* parent = nullptr);
+
+  /**
+   * @brief Destructor
+   */
   ~MainWindow();
 
     private slots:
 
+  /**
+   * @brief This method is called when the water button is clicked. For now, it
+   * launches Gazebo to show the demo.
+   */
   void on_water_btn_clicked();
 
     private:
