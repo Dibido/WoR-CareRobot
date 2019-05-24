5bc94aa09eab810067150ab0fa357ea8da4b23c4
Gianni Monteban <G.Monteban@student.han.nl>
2019-05-24 15:23:13 +0200

    EBGURG-374 removed cout

--- kinematica/controller/src/controller/SoftStop.cpp
+++ kinematica/controller/src/controller/SoftStop.cpp
@@ -14,17 +14,14 @@ namespace controller
 
   void SoftStop::entryAction(Context* context)
   {
-    // std::cout << __PRETTY_FUNCTION__ << std::endl;
   }
 
   void SoftStop::doActivity(Context* context)
   {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
     context->setState(std::make_shared<Init>());
   }
 
   void SoftStop::exitAction(Context* context)
   {
-    // std::cout << __PRETTY_FUNCTION__ << std::endl;
   }
 } // namespace controller
