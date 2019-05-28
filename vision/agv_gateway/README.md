751d0d95858fc8a484d106fca3f8e5e811495345
Dibran Dokter <HS.Dokter@student.han.nl>
2019-05-28 20:38:02 +0200

    EBGURG-390: Added agv_gateway and agv_parser

---
+++ vision/agv_gateway/README.md
@@ -0,0 +1,5 @@
+# Agv Gateway
+
+Code for the arduino gateway that reads the message from the NRF and writes it to the serial
+
+To execute this code upload it to an Arduino Mini with an NRF24 installed.
---
+++ vision/agv_parser/src/agv_parser/AgvParser.cpp
@@ -0,0 +1,81 @@
+#include "agv_parser/AgvParser.hpp"
+
+void agv_parser::AgvParser::Run()
+{
+  // Open serial
+  boost::system::error_code lBoostError;
+  // mSerial.open(mSerialPort, lBoostError);
+  // if (lBoostError)
+  // {
+  //   std::cout << "error : port.open() failed" << std::endl;
+  //   return;
+  // }
+  while (true)
+  {
+    // Get message
+    std::string lRecievedMessage = readLine();
+    // Parse message
+    sensor_interfaces::AGVSpeed lAgvMessage =
+        parseRecievedMessage(lRecievedMessage);
+    // Publish to ROS topic
+    mAgvPublisher.publish(lAgvMessage);
+    std::cout << "Handled message : " << lRecievedMessage << std::endl;
+  }
+}
+
+agv_parser::AgvParser::AgvParser(std::string aPort)
+    : mIoService(), mSerial(mIoService, aPort)
+{
+  // Set up serial
+  mSerialPort = aPort;
+  mSerial.set_option(boost::asio::serial_port_base::baud_rate(9600));
+  // Set up ROS
+  // Create a ros NodeHandle
+  mRosNode = std::make_unique<ros::NodeHandle>();
+  // Advertise ROS node
+  mAgvPublisher =
+      mRosNode->advertise<sensor_interfaces::AGVSpeed>("/sensor/agv", 1);
+}
+
+agv_parser::AgvParser::~AgvParser()
+{
+  // Tear down serial
+}
+
+std::string agv_parser::AgvParser::readLine()
+{
+  // Reading data char by char, code is optimized for simplicity, not speed
+  char c;
+  std::string result;
+  for (;;)
+  {
+    boost::asio::read(mSerial, boost::asio::buffer(&c, 1));
+    switch (c)
+    {
+    case '\r':
+      break;
+    case '\n':
+      return result;
+    default:
+      result += c;
+    }
+  }
+}
+
+sensor_interfaces::AGVSpeed
+    agv_parser::AgvParser::parseRecievedMessage(std::string aRecievedMessage)
+{
+  sensor_interfaces::AGVSpeed lAgvMessage;
+  // Parse the command
+  // Example command = "#S#0.23131\n"
+  // Strip the #S part from the message
+  std::string lStrippedHashString = aRecievedMessage.substr(
+      aRecievedMessage.find('#') + 1, aRecievedMessage.size());
+  // Read the value after the #
+  std::string lSpeedString = lStrippedHashString.substr(
+      aRecievedMessage.find('#') + 2, aRecievedMessage.size());
+  float lAgvSpeed = static_cast<float>(atof(lSpeedString.c_str()));
+  // Fill message
+  lAgvMessage.speed = lAgvSpeed;
+  return lAgvMessage;
+}
