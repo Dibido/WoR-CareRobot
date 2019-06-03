e9a17ba237aa6e80d9af8142abab3cd1010b1655
Martijn Vogelaar <MJ.Vogelaar@student.han.nl>
2019-06-03 15:44:36 +0200

    EBGURG-403 Add Unittest

--- kinematica/controller/unitTests/arrivalTime.cpp
+++ kinematica/controller/unitTests/arrivalTime.cpp
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 
-TEST(ArrivalTime, calculateArrivalTime)
+TEST(ArrivalTime, singleJointChanged)
 {
   ros::Time::init();
   kinematics::Configuration lCurrentConfiguration = kinematics::Configuration();
@@ -24,5 +24,30 @@ TEST(ArrivalTime, calculateArrivalTime)
                 ,
             ros::Time::now());
   EXPECT_LT(lTrajectoryProvider
-                .calculateArrivalTime(lCurrentConfiguration, lGoalConfiguration), ros::Time::now());
+                .calculateArrivalTime(lCurrentConfiguration, lGoalConfiguration), ros::Time::now()+ros::Duration(1));
+}
+
+TEST(ArrivalTime, multipleJointsChanged)
+{
+  ros::Time::init();
+  kinematics::Configuration lCurrentConfiguration = kinematics::Configuration();
+  lCurrentConfiguration.setTheta(0, 0);
+  lCurrentConfiguration.setTheta(1, 10);
+  lCurrentConfiguration.setTheta(2, 20);
+  lCurrentConfiguration.setTheta(3, 30);
+  lCurrentConfiguration.setTheta(4, 40);
+  lCurrentConfiguration.setTheta(5, 50);
+  lCurrentConfiguration.setTheta(6, 60);
+  kinematics::Configuration lGoalConfiguration = lCurrentConfiguration;
+    lGoalConfiguration.setTheta(5, 10);
+  lGoalConfiguration.setTheta(6, 100);
+  controller::TrajectoryProvider lTrajectoryProvider =
+      controller::TrajectoryProvider();
+
+  EXPECT_GT(lTrajectoryProvider
+                .calculateArrivalTime(lCurrentConfiguration, lGoalConfiguration)
+                ,
+            ros::Time::now());
+  EXPECT_LT(lTrajectoryProvider
+                .calculateArrivalTime(lCurrentConfiguration, lGoalConfiguration), ros::Time::now()+ros::Duration(1));
 }
