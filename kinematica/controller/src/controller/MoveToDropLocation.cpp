72e4d4aa46885e12ff7c7d11e8cf7d233de7a876
Gianni Monteban <G.Monteban@student.han.nl>
2019-06-04 09:51:44 +0200

    EBGURG-393 - fixed emergency stop and solved some warnings

--- kinematica/controller/src/controller/MoveToDropLocation.cpp
+++ kinematica/controller/src/controller/MoveToDropLocation.cpp
@@ -22,32 +22,37 @@ namespace controller
 
   void MoveToDropLocation::doActivity(Context* aContext)
   {
-    if (mArrivalTime.sleepUntil(mArrivalTime))
+    int64_t lMovementDuration_ns =
+        mArrivalTime.toNSec() - ros::Time::now().toNSec();
+    if (lMovementDuration_ns > 0)
     {
-      if (mTrajectory.size() == 0)
-      {
-        aContext->setState(std::make_shared<WaitForReleaseSignal>());
-        return;
-      }
-      else
-      {
-        kinematics::Configuration& lTargetConfiguration = mTrajectory.front();
-        aContext->robotControl()->publish(cSpeedFactor, lTargetConfiguration);
-        mArrivalTime = mTrajectoryProvider.calculateArrivalTime(
-            aContext, lTargetConfiguration);
-        aContext->configuration() = lTargetConfiguration;
-        ROS_DEBUG(
-            "Move to \n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f",
-            lTargetConfiguration[0], lTargetConfiguration[1],
-            lTargetConfiguration[2], lTargetConfiguration[3],
-            lTargetConfiguration[4], lTargetConfiguration[5],
-            lTargetConfiguration[6]);
-        mTrajectory.pop();
-      }
+      std::this_thread::sleep_for(
+          std::chrono::nanoseconds(lMovementDuration_ns));
     }
-  }
+    if (mTrajectory.size() == 0)
+    {
+      aContext->setState(std::make_shared<WaitForReleaseSignal>());
+      return;
+    }
+    else
+    {
+      kinematics::Configuration& lTargetConfiguration = mTrajectory.front();
+      aContext->robotControl()->publish(cSpeedFactor, lTargetConfiguration);
+      mArrivalTime = mTrajectoryProvider.calculateArrivalTime(
+          aContext, lTargetConfiguration);
+      aContext->configuration() = lTargetConfiguration;
+      ROS_DEBUG(
+          "Move to \n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f\n- %.4f",
+          lTargetConfiguration[0], lTargetConfiguration[1],
+          lTargetConfiguration[2], lTargetConfiguration[3],
+          lTargetConfiguration[4], lTargetConfiguration[5],
+          lTargetConfiguration[6]);
+      mTrajectory.pop();
+    }
 
-  void MoveToDropLocation::exitAction(Context* aContext)
+  } // namespace controller
+
+  void MoveToDropLocation::exitAction(Context*)
   {
     while (mTrajectory.empty() == false)
     {
