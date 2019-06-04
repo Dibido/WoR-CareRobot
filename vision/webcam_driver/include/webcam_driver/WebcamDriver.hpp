d297c1858eff290ec2e8c652c2159dcd67eb6b6a
Casper Uiterwijk <C.Uiterwijk@student.han.nl>
2019-06-04 11:23:07 +0200

    EBGURG-416 Implement WebcamDriver and publish image data on /sensor/webcam

--- vision/webcam_driver/include/webcam_driver/WebcamDriver.hpp
+++ vision/webcam_driver/include/webcam_driver/WebcamDriver.hpp
@@ -4,7 +4,6 @@
 #include <boost/optional.hpp>
 #include <iostream>
 #include <math.h>
-#include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
 namespace webcam_driver
@@ -12,8 +11,23 @@ namespace webcam_driver
   class WebcamDriver
   {
       public:
-    WebcamDriver();
+    WebcamDriver(unsigned int aWebcamId);
     ~WebcamDriver();
+    /*
+     * @brief Initialises the webcam device for the maximum resolution.
+     *
+     * @return Whether the initialisation was successful.
+     */
+    bool initialise();
+    /**
+     * @brief Captures a frame from the webcam.
+     *
+     * @return The captured frame.
+     */
+    cv::Mat captureFrame();
+
+      private:
+    cv::VideoCapture mWebcamDevice;
   };
 } // namespace webcam_driver
 
--- vision/webcam_driver/src/main.cpp
+++ vision/webcam_driver/src/main.cpp
@@ -1,3 +1,4 @@
+#include "webcam_driver/WebcamDriver.hpp"
 #include <ctime>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
@@ -7,28 +8,68 @@
 
 namespace webcam_driver_constants
 {
-
-  const std::string cWebcamTopic = "/sensor/webcam/img_raw";
+  const std::string cWebcamTopic = "/sensor/webcam";
   const std::string cComponentName = "webcam_driver";
+  const unsigned int cRefreshRate = 30;
 } // namespace webcam_driver_constants
 
 int main(int argc, char** argv)
 {
-
   ros::init(argc, argv, webcam_driver_constants::cComponentName);
-  ros::NodeHandle nh;
+  ros::NodeHandle lNodeHandle;
 
-  ros::Rate loop_rate(10);
+  unsigned int lWebcamId = 0;
+  if (argc > 1 && std::string(argv[1]) != "-h" && argv[1][0] == '-')
+  {
+    lWebcamId = static_cast<unsigned int>(atoi(argv[1] + 1));
+  }
+  else
+  {
+    ROS_INFO_STREAM("Usage: webcam_driver -<webcam device id> [-d]");
+    return 1;
+  }
 
-  image_transport::ImageTransport it(nh);
-  const std::string cTopicName = webcam_driver_constants::cWebcamTopic;
-  /* image_transport::Subscriber sub = it.subscribe(cTopicName, 1,
-   * imageCallback); */
+  bool lDebug = false;
+  if (argc > 2 && std::string(argv[2]) == "-d")
+  {
+    lDebug = true;
+  }
 
+  webcam_driver::WebcamDriver lWebcamDriver(lWebcamId);
+  if (!lWebcamDriver.initialise())
+  {
+    ROS_WARN_STREAM("Failed to initialise webcam with id " << lWebcamId);
+    return 2;
+  }
+
+  image_transport::ImageTransport lImageTransport(lNodeHandle);
+  image_transport::Publisher lPublisher =
+      lImageTransport.advertise(webcam_driver_constants::cWebcamTopic, 1);
+
+  if (lDebug)
+  {
+    cv::namedWindow("a", cv::WINDOW_AUTOSIZE);
+  }
+  cv::Mat lFrame;
   while (ros::ok())
   {
-    loop_rate.sleep();
+    cv::Mat lCapturedFrame = lWebcamDriver.captureFrame();
+    sensor_msgs::ImagePtr lMsg =
+        cv_bridge::CvImage(std_msgs::Header(), "bgr8", lCapturedFrame)
+            .toImageMsg();
+    lPublisher.publish(lMsg);
     ros::spinOnce();
+
+    if (lDebug)
+    {
+      cv::imshow("a", lWebcamDriver.captureFrame());
+    }
+
+    int lChar = cv::waitKey(1000 / webcam_driver_constants::cRefreshRate);
+    if (lChar == 27)
+    {
+      break;
+    }
   }
 
   return 0;
