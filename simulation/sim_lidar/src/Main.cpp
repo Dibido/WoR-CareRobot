f10ad5f4663ac5ed91e9260d3b534f88d18704ca
Stein Zwerink <SRH.Zwerink@student.han.nl>
2019-06-07 11:42:02 +0200

    EBGURG-405 using relative path

--- simulation/sim_lidar/src/Main.cpp
+++ simulation/sim_lidar/src/Main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv)
   std::vector<double> measurements;
   calculate::Calculatedata lCalc;
   lCalc.processData(
-      "/home/stein/1_ws/src/wor-18-19-s2/simulation/sim_lidar/src/"
-      "lidarData.txt");
+      "src/wor-18-19-s2/simulation/sim_lidar/src/lidarData.txt");
 
   return 0;
 }
+
