aaffa65807260610df6af7982e32f0b9d0067794
Thomas de Winkel <TDJ.deWinkel@student.han.nl>
2019-06-07 12:01:32 +0200

    EBGURG-463 fixed the base of implementing the interface in the kinect

--- simulation/sim_kinect/include/sim_kinect/KinectPlugin.hpp
+++ simulation/sim_kinect/include/sim_kinect/KinectPlugin.hpp
@@ -2,34 +2,69 @@
 #ifndef PROJECT_KINECTPLUGIN_HPP
 #define PROJECT_KINECTPLUGIN_HPP
 
+#include "ros/callback_queue.h"
+#include "std_msgs/String.h"
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
 #include <gazebo_plugins/gazebo_ros_openni_kinect.h>
+#include <ros/subscribe_options.h>
+#include <string>
+#include <thread>
+
+#include "environment_controller/IObstacles.hpp"
+#include "environment_controller/Object.hpp"
+#include "environment_controller/Position.hpp"
+#include <kinematica_msgs/Object.h>
+#include <kinematica_msgs/Obstacles.h>
 
 namespace gazebo
 {
-/**
- * Plugin class for the Kinect sensor to pull data from the sensor and put it on a ROS topic
- */
-class KinectPlugin : public GazeboRosOpenniKinect
-{
-public:
   /**
-   * Default constructor
+   * Plugin class for the Kinect sensor to pull data from the sensor and put it
+   * on a ROS topic
    */
-  KinectPlugin() = default;
+  class KinectPlugin : public GazeboRosOpenniKinect,
+                       public environment_controller::IObstacles
+  {
+      public:
+    /**
+     * Default constructor
+     */
+    KinectPlugin() = default;
 
-  /**
-   * Default destructor
-   */
-  virtual ~KinectPlugin() = default;
+    /**
+     * Default destructor
+     */
+    virtual ~KinectPlugin() = default;
 
-  /**
-   * Setup the sensor plugin. Gets executed when the model (SDF file) gets loaded
-   * @param sensor pointer to the sensor
-   * @param sdf pointer to the sdf (defined in the model file) element of the sensor
-   */
-  virtual void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf);
-};
+    /**
+     * Setup the sensor plugin. Gets executed when the model (SDF file) gets
+     * loaded
+     * @param sensor pointer to the sensor
+     * @param sdf pointer to the sdf (defined in the model file) element of the
+     * sensor
+     */
+    virtual void Load(sensors::SensorPtr aSensor, sdf::ElementPtr aSdf);
 
-}  // namespace gazebo
+    /**
+     * @brief the function to implement for the interface
+     *
+     * @param aObstacles The obstacles that are found
+     */
+    void passObstacles(
+        const environment_controller::Obstacles& aObstacles) override;
+
+      private:
+    // ROS
+    ros::NodeHandlePtr mRosNode;
+    ros::Publisher mKinectPublisher;
+    ros::CallbackQueue mRosQueue;
+    std::thread mRosQueueThread;
+  };
+
+} // namespace gazebo
 
 #endif
--- simulation/sim_kinect/src/KinectPlugin.cpp
+++ simulation/sim_kinect/src/KinectPlugin.cpp
@@ -2,10 +2,28 @@
 
 namespace gazebo
 {
-void KinectPlugin::Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf)
-{
-  GazeboRosOpenniKinect::Load(sensor, sdf);
-}
+  const std::string cKinectPublishTopic = "/sensor/kinect/obstacles";
+  const std::string cImgSubscribeTopic = "/sensor/kinect/img_raw";
 
-GZ_REGISTER_SENSOR_PLUGIN(KinectPlugin);
-}  // namespace gazebo
+  void KinectPlugin::Load(sensors::SensorPtr aSensor, sdf::ElementPtr aSdf)
+  {
+    GazeboRosOpenniKinect::Load(aSensor, aSdf);
+  }
+
+  void KinectPlugin::passObstacles(
+      const environment_controller::Obstacles& aObstacles)
+  {
+    if (!ros::isInitialized())
+    {
+      int argc = 0;
+      char** argv = nullptr;
+      ros::init(argc, argv, "AGV_model_plugin",
+                ros::init_options::NoSigintHandler);
+    }
+
+    mKinectPublisher = mRosNode->advertise<kinematica_msgs::Obstacles>(
+        gazebo::cKinectPublishTopic, 1);
+  }
+
+  GZ_REGISTER_SENSOR_PLUGIN(KinectPlugin);
+} // namespace gazebo
