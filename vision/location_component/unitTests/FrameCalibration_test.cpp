62fd76167e3809da6814b7f1c34b46e2b833bd26
Hendrik Nusselder <dh.nusselder@student.han.nl>
2019-06-07 13:30:25 +0200

    EBGURG-419 Created unit test for framecalibration

--- vision/location_component/unitTests/FrameCalibration_test.cpp
+++ vision/location_component/unitTests/FrameCalibration_test.cpp
@@ -1,5 +1,8 @@
 #include "ImagePath.hpp"
 #include <gtest/gtest.h>
+
+#include "location_component/FrameCalibration.hpp"
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -7,9 +10,16 @@
 TEST(DetectAGVSuite, CalibrateAGVFrame)
 {
   cv::Mat lImage =
-      cv::imread(getImagePath("Test_picture_frame.png"), cv::IMREAD_COLOR);
+      cv::imread(getImagePath("Test_picture_frame.jpg"), cv::IMREAD_COLOR);
   cv::Mat lMat;
-  lImage.copyTo(lMat);
 
-  imshow("test", lMat);
+  location_component::AGVFrameCalibration lAGVFrameCalibration(false);
+  location_component::FrameCalibration lFrameCalibration(lAGVFrameCalibration);
+
+  lFrameCalibration.removeEverythingButAGV(lImage, lMat);
+
+  unsigned int lTotal = lMat.rows * lMat.cols;
+  unsigned int lblackPixels = lTotal - countNonZero(lMat);
+  
+  EXPECT_EQ(10088580, lblackPixels);
 }
