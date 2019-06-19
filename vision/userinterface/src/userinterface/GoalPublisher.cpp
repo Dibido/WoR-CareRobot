1d4de8c9a314010eda100a81c5fcc844252bf7cf
Dibran Dokter <HS.Dokter@student.han.nl>
2019-06-19 17:42:17 +0200

    EBGURG-406: fixed review comments

--- vision/userinterface/src/userinterface/GoalPublisher.cpp
+++ vision/userinterface/src/userinterface/GoalPublisher.cpp
@@ -20,7 +20,7 @@ namespace userinterface
     // Create and fill message with aPosition.
     kinematica_msgs::Goal lMsg;
 
-    lMsg.staticCup = false;
+    lMsg.staticGoal = false;
     lMsg.position.x = aPosition.x_m();
     lMsg.position.y = aPosition.y_m();
     lMsg.position.z = aPosition.z_m();
