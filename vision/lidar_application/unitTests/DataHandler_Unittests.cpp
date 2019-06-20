70db00e4e836deda750a15d8cbdb3b50353246f3
Maurits Muijsert <MPW.Muijsert@student.han.nl>
2019-06-20 11:54:01 +0200

    EBGURG-481 added whitespace

--- vision/lidar_application/unitTests/DataHandler_Unittests.cpp
+++ vision/lidar_application/unitTests/DataHandler_Unittests.cpp
@@ -60,6 +60,7 @@ TEST(DataHandler, GetNewData)
   // There should be no 'new' data available
   EXPECT_FALSE(lDataHandler.isNewDataAvailable());
 }
+
 TEST(DataHandler, PublishData)
 {
   DataHandlerDerived lDataHandler;
